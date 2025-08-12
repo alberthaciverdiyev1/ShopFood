eac6acf16b02472e1ebca224f1bfebaffa0d5a07
arzummammadova <arzuam-af106@code.edu.az>
2025-08-12 22:59:39 +0400

    navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,11 +1,89 @@
-<nav>
-    <ul>
-        <li>
-            <a class="  py-9 px-4 bg-black" href="index.php">Home</a>
-        </li>
-        <li>
-            <a  href="index.php?controller=product&action=index">Pro</a>
-        </li>
-       
-    </ul>
+<nav class="flex justify-between items-center px-6 py-4  shadow-md">
+  <ul class="flex items-center space-x-6 gap-5">
+    <li>
+      <a href="/" class="py-2 px-4 bg-black  font-bold rounded">
+         Logo
+      </a>
+      <a href="/">
+      <i class="fa-brands fa-telegram"></i>
+      </a>
+      <a href="">
+      <i class="fa-brands fa-whatsapp"></i>
+      </a>
+    </li>
+   
+  </ul>
+
+  <ul class="flex items-center space-x-4 gap-4">
+    <li >
+        <select name=" language" id="">
+            <option value="">
+                <i class="fa-solid fa-globe"></i>
+                <span>En</span>
+                <i class="fa-solid fa-caret-down"></i>
+                <ul class="absolute bg-white shadow-md rounded-md">
+                    <li
+                
+
+            </option>
+        </select>
+     
+    </li>
+    <li>
+      <a href="#" class=" hover:text-black">
+        <i class="fas fa-shopping-cart"></i>
+      </a>
+    </li>
+    <li>
+        <a href="">
+        <i class="fa-regular fa-user"></i>
+        </a>
+    </li>
+    <li>
+      1524
+    </li>
+
+    <li>
+        <a href="">
+        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+        </a>
+    </li>
+  </ul>
+
+
+  <div class="menu hidden">
+  <i class="fa-solid fa-bars"></i>
+
+  </div>
+  <div class="menu hidden">
+    <ul class="items-center space-x-4 gap-4">
+      <li>
+        <a href="" class="hover:text-black">
+          <i class="fa-solid fa-house"></i>
+        </a>
+      </li
+      >
+      <li>
+        <a href="" class="hover:text-black">
+          <i class="fa-solid fa-user"></i>
+        </a>
+      </li>
+      <li>
+        <a href="" class="hover:text-black">
+          <i class="fa-solid fa-cart-shopping"></i>
+        </a>
+      </li>
+      <li>
+        <a href="" class="hover:text-black">
+          <i class="fa-solid fa-globe"></i>
+        </a>
+      </li>
+      <li>
+        <a href="" class="hover:text-black">
+          <i class="fa-solid fa-arrow-right-from-bracket"></i>
+        </a>
+      </li>
+    </ul
+        </a>
+  </div>
 </nav>
