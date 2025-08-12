2723384b77c8755819c392efbe3d1a081141e37c
arzummammadova <arzuam-af106@code.edu.az>
2025-08-12 22:35:03 +0400

    test fot pushing front branch

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav>
     <ul>
         <li>
-            <a class=" text-white py-9 px-4 bg-black" href="index.php">Home</a>
+            <a class="  py-9 px-4 bg-black" href="index.php">Home</a>
         </li>
         <li>
-            <a href="index.php?controller=product&action=index">Products</a>
+            <a  href="index.php?controller=product&action=index">Pro</a>
         </li>
        
     </ul>
