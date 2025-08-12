36d93f7993b87e3726d017b4f0aeee36b14e4fea
arzummammadova <arzuam-af106@code.edu.az>
2025-08-12 23:54:55 +0400

    hero

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,8 +2,8 @@
 <section class="hero-gradient">
 <div class="px-4 relative  flex justify-between h-100vh ">
     <img class=" h-full absolute left-0  z-0" src="{{ asset ('images/hero-left.png') }}" alt="">
-    <div class=" flex flex-col gap-5 text-start items-center justify-center">
-    <h1  class="text-6xl font-bold z-2 text-white ">
+    <div class=" w-[43%]  flex flex-col gap-5 text-start items-center justify-center">
+    <h1  class="text-6xl font-bold z-2 text-white  ">
     Сливочное масло тепер 
     по выгодной цене
     </h1>
