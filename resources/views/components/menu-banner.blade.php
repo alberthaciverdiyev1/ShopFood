a25af8bd3dc448fbd9efd03c73957f70bccf4708
arzummammadova <arzuam-af106@code.edu.az>
2025-08-17 20:02:56 +0400

    new section

--- resources/views/components/menu-banner.blade.php
+++ resources/views/components/menu-banner.blade.php
@@ -1,7 +1,7 @@
-<div class=" mx-auto px-4">
+<div class=" mx-auto px-4 ">
     <p class="font-bold ">Меню</p>
-    <div class=" mt-4 flex w-full gap-4">
-        <div class="bg-[var(--color-secondary)] rounded-2xl px-7 py-3">
+    <div class=" mt-4 flex flex-wrap gap-3">
+        <div class="bg-[var(--color-secondary)]   rounded-2xl px-7 py-3">
             <span class="text-center text-white text-bold ">Заказ</span>
 
         </div>
